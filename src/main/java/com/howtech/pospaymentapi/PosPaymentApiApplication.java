bcccaf21170cc27ab1b5b2ba58bc395f950c42c1
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:19:02 -0500

    initial commit

---
+++ src/main/java/com/howtech/pospaymentapi/PosPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.pospaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosPaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/pospaymentapi/PosPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.pospaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
